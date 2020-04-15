379eaaa49c3dd8f31c9128ae6bba966779e17a70
Meg Gutshall <meghan.gutshall@gmail.com>
2020-04-15 17:02:47 -0400

    Build from React TTT study group

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      board: [
+        '',
+        '',
+        'O',
+        'X',
+        '',
+        '',
+        'X',
+        '',
+        ''
+      ],
+    }
+  }
+
+  handleClick = (cellIndex) => {
+    const cloneBoard = this.state.board.slice()
+    cloneBoard[cellIndex] = "X"
+
+    this.setState(() => (
+      {
+        board: cloneBoard
+      }
+    ))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">Welcome to Tic-Tac-Toe</header>
+        <div id="board">
+        { this.state.board.map((cell, index) => (
+          <p key={index} className="cell" onClick={() => this.handleClick(index)}>
+            {cell}
+          </p>
+        ))}
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
